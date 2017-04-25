dde427d59992a8ba0bb3deed8a0d050b203ad926
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-04-25 15:18:49 +0100

    Setup Sinon and Mocha/Chai - faux test papassing

---
+++ spec/test.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var assert = require('assert')
+var User = require('../src/Search.js')
+
+describe('Search', () => {
+  it('should export a function', () => {
+    assert.equal(1, 1)
+  })
+})
