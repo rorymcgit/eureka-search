721d0778bbfdd9e2667f9d0eba4725f4779883a7
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-04-26 09:39:15 +0100

    WIP

---
+++ spec/SearchSpec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var assert = require('assert')
+var Search = require('../src/Search.js')
+
+describe('Search', () => {
+  it('should have a function hello', () => {
+    assert.equal(Search.hello, "hello")
+  })
+})
