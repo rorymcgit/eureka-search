035693857775495f5d793109476d458966e81849
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-04-26 15:17:09 +0100

    accept query from url and return array of links to page

---
+++ controllers/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+var pgp = require('pg-promise')(/*options*/);
+var db = pgp('postgres://localhost/app_test');
+
+app.get('/', function (req, res) {
+  res.send('Search by appending the url with your query');
+});
+
+app.get('/:myquery', function (req, res) {
+  db.many("SELECT url FROM links WHERE title = '" + req.params.myquery + "'")
+    .then(function (data) {
+      var urls = [];
+      for(var i = 0; i < data.length; i++) {
+        urls.push(data[i].url);
+      }
+      res.send(urls);
+    })
+    .catch(function (error) {
+      console.log('ERROR:', error);
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
