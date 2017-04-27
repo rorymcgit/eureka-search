83179d91774f3e2a9ca8cefae8a653f7a62ba101
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-04-27 12:56:09 +0100

    Add basic error handling for query result error

--- controllers/routes/getLinks.js
+++ controllers/routes/getLinks.js
@@ -1,8 +1,13 @@
 var retrieval = require('../../models/urlRetrieval');
+
 var db = require('../../db');
+var pgp = require('pg-promise')({noLocking:true});
+var QueryResultError = pgp.errors.QueryResultError;
+var qrec = pgp.errors.queryResultErrorCode;
+
 
 function getLinks(req, res) {
-  if (req.params.query !== 'favicon.ico'){
+  if(req.params.query !== 'favicon.ico'){
     /// many, then and catch is from the promise library
     /// 'many' retrieves from db urls where search term matches links 'title'(db)
     db.many("SELECT url FROM links WHERE title = '" + req.params.query + "'")
@@ -13,7 +18,12 @@ function getLinks(req, res) {
       /// catching any errors e.g query not in database
       /// will be improved later, to inform user
       .catch(function (error) {
-        console.log('ERROR:', error);
+        console.log('The error is:\n', error);
+        // console.log(qrec);
+        // if the error code you get matches the error code of 'no data retrieved'
+        if(error.code === qrec.noData) {
+          res.send("no links match your query");
+        }
     });
   }
 }
