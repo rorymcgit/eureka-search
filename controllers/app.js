6835843f2c068c234f1f1b2abb00fcd84b1c76ee
Vicky Ledsom <victorialedsom@gmail.com>
2017-04-27 12:32:32 +0100

    Add comments for deeper understanding

--- controllers/app.js
+++ controllers/app.js
@@ -3,9 +3,11 @@ const app = express();
 const getLinks = require('./routes/getLinks');
 
 app.get('/', function (req, res) {
+  /// response being sent to the user
   res.send("Hello, you're home");
 });
 
+/// takes users query and calls on getLinks.js
 app.get('/:query', getLinks);
 
 app.listen(3000, function () {
--- controllers/routes/getLinks.js
+++ controllers/routes/getLinks.js
@@ -3,11 +3,15 @@ var db = require('../../db');
 
 function getLinks(req, res) {
   if (req.params.query !== 'favicon.ico'){
+    /// many, then and catch is from the promise library
+    /// 'many' retrieves from db urls where search term matches links 'title'(db)
     db.many("SELECT url FROM links WHERE title = '" + req.params.query + "'")
       .then(function (data) {
         var urls = retrieval(data);
         res.send(urls.join("</br>"));
       })
+      /// catching any errors e.g query not in database
+      /// will be improved later, to inform user
       .catch(function (error) {
         console.log('ERROR:', error);
     });
