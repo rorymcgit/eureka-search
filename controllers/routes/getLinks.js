6aa4c7bc24c0bc0db26d1d92e39e33648e59dcbe
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2017-04-27 12:27:16 +0100

    Update getLinks.js

--- controllers/routes/getLinks.js
+++ controllers/routes/getLinks.js
@@ -1,13 +1,15 @@
 var db = require('../../db.js');
 
 function getLinks(req, res) {
-  db.map('SELECT url FROM links WHERE title = ${query}', req.params, a => a.url)
-    .then(function (urls) {
-      res.send(urls.join("</br>"));
-    })
-    .catch(function (error) {
-      console.log('ERROR:', error);
-  });
+	if (req.params.query !== 'favicon.ico') {
+		db.map('SELECT url FROM links WHERE title = ${query}', req.params, a => a.url)
+			.then(function (urls) {
+				res.send(urls.join("</br>"));
+			})
+			.catch(function (error) {
+				console.log('ERROR:', error);
+			});
+	}
 }
 
 module.exports = getLinks;
