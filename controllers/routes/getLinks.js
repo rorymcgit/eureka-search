8bf531f4e2b1f8a23bf5ce4e7ba9bb48e99d126a
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-04-29 12:46:53 +0100

    Fix map method to call on weburl not url

--- controllers/routes/getLinks.js
+++ controllers/routes/getLinks.js
@@ -4,18 +4,18 @@ var QueryResultError = pgp.errors.QueryResultError;
 var qrec = pgp.errors.queryResultErrorCode;
 
 function getLinks(req, res) {
-	if(req.params.query !== 'favicon.ico') {
-		db.map('SELECT url FROM links WHERE title = ${query}', req.params, a => a.url)
+	// if(req.params.query !== 'favicon.ico') {
+		db.map('SELECT weburl FROM weburlsandcontent WHERE title = ${query}', req.params, a => a.weburl)
 			.then(function (urls) {
 				res.send(urls.join("</br>"));
 			})
 			.catch(function (error) {
         console.log('ERROR:\n', error);
         if(error.code === qrec.noData) {
-          res.send("no links match your query");
+          res.send("No links match your query.");
 			}
 		});
-	}
+	// }
 }
 
 module.exports = getLinks;
