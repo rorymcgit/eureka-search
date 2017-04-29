80f7bf21ef1aa60b593b74425f3aa7eb44bbe9f4
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-04-29 12:47:40 +0100

    Add back in guard clause for favicon

--- controllers/routes/getLinks.js
+++ controllers/routes/getLinks.js
@@ -4,7 +4,7 @@ var QueryResultError = pgp.errors.QueryResultError;
 var qrec = pgp.errors.queryResultErrorCode;
 
 function getLinks(req, res) {
-	// if(req.params.query !== 'favicon.ico') {
+	if(req.params.query !== 'favicon.ico') {
 		db.map('SELECT weburl FROM weburlsandcontent WHERE title = ${query}', req.params, a => a.weburl)
 			.then(function (urls) {
 				res.send(urls.join("</br>"));
@@ -15,7 +15,7 @@ function getLinks(req, res) {
           res.send("No links match your query.");
 			}
 		});
-	// }
+	}
 }
 
 module.exports = getLinks;
