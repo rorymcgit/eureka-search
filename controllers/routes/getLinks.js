aadbdd027fc5853e949bcf09f17cfa71aed8bc32
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-04-29 11:49:49 +0100

    Delete urlRetrieval and its tests. Function not required thanks to Vitaly

--- controllers/routes/getLinks.js
+++ controllers/routes/getLinks.js
@@ -4,13 +4,13 @@ var QueryResultError = pgp.errors.QueryResultError;
 var qrec = pgp.errors.queryResultErrorCode;
 
 function getLinks(req, res) {
-	if (req.params.query !== 'favicon.ico') {
+	if(req.params.query !== 'favicon.ico') {
 		db.map('SELECT url FROM links WHERE title = ${query}', req.params, a => a.url)
 			.then(function (urls) {
 				res.send(urls.join("</br>"));
 			})
 			.catch(function (error) {
-        console.log('The error is:\n', error);
+        console.log('ERROR:\n', error);
         if(error.code === qrec.noData) {
           res.send("no links match your query");
 			}
