1b1267f2bd93e2350f494626fedd7d767b856bf5
Vicky Ledsom <victorialedsom@gmail.com>
2017-05-01 13:44:15 +0100

    (WIP) Attempting to connect search view with express post requests

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const app = express();
-const getLinks = require('./controllers/routes/getLinks');
+const express = require('express'),
+      getLinks = require('./controllers/routes/getLinks'),
+      bodyParser = require('body-parser'),
+      app = express();
 
-// set the view engine to ejs
+      // body parser- parse incoming request bodies in a middleware before your handlers, available under the re.body property.
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+//search engine view to ejs
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-
 app.get('/', function (req, res) {
-  /// response being sent to the user
-  // res.send("Hello, you're home");
   res.render('search');
 });
 
@@ -17,13 +22,20 @@ app.get('/about', function(req, res) {
     res.render('about');
 });
 
+app.post('/search', function(req, res) {
+  var query = req.body.searchinput;
+  console.log(req.body.searchinput);
+  res.redirect(307, '/' + req.body.searchinput, getLinks);
+});
+
+// app.get('/:query', getLinks);
+
 // Use below to ignore favicon request? Or use guard in getLinks.js?
 // app.get('/favicon.ico', function(req, res) {
 //     res.sendStatus(204);
 // });
-
+// app.post('search', )
 /// takes users query and calls on getLinks.js
-app.get('/:query', getLinks);
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!');
--- controllers/routes/getLinks.js
+++ controllers/routes/getLinks.js
@@ -6,10 +6,11 @@ var qrec = pgp.errors.queryResultErrorCode;
 
 function getLinks(req, res) {
 	// need to find out why favicon and style are treated as queries
-	if(req.params.query !== 'favicon.ico' && req.params.query !== 'style.css') {
+	if(req.params.query !== 'favicon.ico' && req.params.query !== 'style.css' && req.params.query !== '307') {
 		db.many('SELECT weburl, title, description FROM weburlsandcontent WHERE title = ${query}', req.params)
 			.then(function (retrieved_data) {
 				res.render('search_results', {results: retrieved_data});
+				console.log(req.params.query);
 			})
 			.catch(function (error) {
         console.log('The error is:\n', error);
