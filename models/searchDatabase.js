4ff443fca7d0105cefff10cdcf27b7f036467716
Vicky Ledsom <victorialedsom@gmail.com>
2017-05-02 12:39:24 +0100

    Add a ability to search all parts of the database

--- models/searchDatabase.js
+++ models/searchDatabase.js
@@ -2,10 +2,17 @@ var db = require('../db.js');
 var pgp = require('pg-promise')({noLocking:true});
 
 function searchDatabase(search) {
-  var database_search = db.any('SELECT * FROM weburlsandcontent WHERE description LIKE ${str}', {
+  var description_search = db.any('SELECT * FROM weburlsandcontent WHERE description LIKE ${str}', {
       str: search
     });
-  return database_search;
+    var title_search = db.any('SELECT * FROM weburlsandcontent WHERE title LIKE ${str}', {
+        str: search
+      });
+      var keyword_search = db.any('SELECT * FROM weburlsandcontent WHERE keywords LIKE ${str}', {
+          str: search
+        });
+
+      return Promise.all([description_search, title_search, keyword_search]);
 }
 
 module.exports = searchDatabase;
