62614edeef421d830985d9ff621172d4291146fd
Vicky Ledsom <victorialedsom@gmail.com>
2017-05-02 16:51:16 +0100

    (WIP) Got search working with array of strings(hard coded) need to implement prepareQueryString

--- models/prepareQueryString.js
+++ models/prepareQueryString.js
@@ -2,30 +2,30 @@ var nonSearchWords = ['a', 'an', 'the', 'is', 'and', 'or', 'www.', 'he', 'she', 
 
 function prepareQueryString(query) {
   splitQueryString(query);
-};
+}
 
 function splitQueryString(query) {
   query = query.split(' ');
   removeNonSearchWords(query);
-};
+}
 
 function removeNonSearchWords(query_array) {
   query_array = difference(query_array, nonSearchWords);
   appendPercentageCharacter(query_array);
-};
+}
 
 function appendPercentageCharacter(query_array) {
   query_array = query_array.map(function(word) {
     return "%" + word + "%";
   });
-  return query_array
-};
+  return query_array;
+}
 
 function difference (query_array, wordsToRemove) {
   return query_array.filter(function(word) {
     return wordsToRemove.indexOf(word) < 0;
   });
-};
+}
 
 module.exports = prepareQueryString;
 
--- models/searchDatabase.js
+++ models/searchDatabase.js
@@ -3,25 +3,28 @@ var pgp = require('pg-promise')({noLocking:true});
 var prepareQueryString = require('./prepareQueryString');
 
 function searchDatabase(search) {
-  var description_search,
-  title_search,
-  keyword_search;
+  var description_search = [];
+  var title_search = [];
+  var keyword_search;
+  
+  search = ["%fish%"];
+  for(i=0; i < search.length; i++){
+    description_search.push(db.any('SELECT * FROM weburlsandcontent WHERE description LIKE ${str}', {
+      str: search[i]
+    }));
+    title_search.push(db.any('SELECT * FROM weburlsandcontent WHERE title LIKE ${str}', {
+      str: search[i]
+    }));
+    keyword_search = db.any('SELECT * FROM weburlsandcontent WHERE keywords LIKE ${str}', {
+      str: search[i]
+    });
+  }
 
-  prepareQueryString(search).then(function(search_array) {
+  console.log("keyword:", keyword_search)
+  console.log("title:", title_search)
+  console.log("desc:", description_search)
 
-    for(var i = 0; i < search_array.length; i++) {
-      description_search = db.any('SELECT * FROM weburlsandcontent WHERE description LIKE ${str}', {
-        str: search_array[i]
-      });
-      title_search = db.any('SELECT * FROM weburlsandcontent WHERE title LIKE ${str}', {
-        str: search_array[i]
-      });
-      keyword_search = db.any('SELECT * FROM weburlsandcontent WHERE keywords LIKE ${str}', {
-        str: search_array[i]
-      });
-    }
-    return Promise.all([description_search, title_search, keyword_search]);
-  });
+  return Promise.all([description_search, title_search, keyword_search]);
 }
 
 module.exports = searchDatabase;
