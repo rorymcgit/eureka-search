9b1aa4f664d4ee6ece62b771379212036670e47c
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-05-03 22:30:30 +0100

    Tidied project files. String modification methods tested and responsiblities deligated

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-const express = require('express'),
-      getLinks = require('./controllers/routes/getLinks'),
-      searchDatabase = require('./models/searchDatabase'),
-      bodyParser = require('body-parser'),
-      db = require('./db.js'),
-      pgp = require('pg-promise')({noLocking:true}),
-      app = express();
+const express = require("express"),
+  getLinks = require("./controllers/routes/getLinks"),
+  searchDatabase = require("./models/searchDatabase"),
+  bodyParser = require("body-parser"),
+  db = require("./db.js"),
+  pgp = require("pg-promise")({ noLocking: true }),
+  app = express();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: true});
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
 
-app.get('/', function (req, res) {
-  res.render('search');
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.render("search");
 });
 
-app.post('/search', urlencodedParser, function(req, res) {
+app.post("/search", urlencodedParser, function(req, res) {
   var query = req.body.searchinput;
-  searchDatabase(query).then(function (returned_data) {
-    res.render('search_results', {data: returned_data, query: query});
-  }).catch(function(e) {
-    console.log(e);
-  });
+  searchDatabase(query)
+    .then(function(returned_data) {
+      res.render("search_results", { data: returned_data, query: query });
+    })
+    .catch(function(e) {
+      console.log(e);
+    });
 });
 
-app.get('/about', function(req, res) {
-    res.render('about');
+app.get("/about", function(req, res) {
+  res.render("about");
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log("I'M LISTENING #3000!");
 });
 
